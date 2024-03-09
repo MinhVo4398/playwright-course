152fc4ae6e3d0b8131109948ab0f3d859693a2d4
Minh Vo <minhvo4398@gmail.com>
2024-03-09 07:26:52 +0700

    remove class from DOM

--- tests/upload.spec.ts
+++ tests/upload.spec.ts
@@ -1,22 +1,53 @@
-import { expect, test } from '@playwright/test';
-const path = require('path');
+import { expect, test } from "@playwright/test";
+const path = require("path");
 
-test.describe('Upload file', () => {
-    test('should upload a test file', async ({ page }) => {
-        // Open Url
-        await page.goto('https://practice.sdetunicorns.com/cart/');
+test.describe("Upload file", () => {
+  test("should upload a test file ", async ({ page }) => {
+    // Open Url
+    await page.goto("https://practice.sdetunicorns.com/cart/");
 
-        //provide test file path
-        const filePath = path.join(__dirname,'../data/logo.png');
+    //provide test file path
+    const filePath = path.join(__dirname, "../data/logo.png");
 
-        //Upload test file (the input must actual input[type=file])
-        await page.setInputFiles('input#upfile_1',filePath);
+    //Upload test file (the input must actual input[type=file])
+    await page.setInputFiles("input#upfile_1", filePath);
 
-        //Click submit button
-        await page.locator('#upload_1').click();
+    //Click submit button
+    await page.locator("#upload_1").click();
 
-        //Assertion
-        await expect(page.locator('#wfu_messageblock_header_1_label_1')).toContainText('uploaded successfully');
-    })
-    
-})
+    //Assertion
+    await expect(
+      page.locator("#wfu_messageblock_header_1_label_1")
+    ).toContainText("uploaded successfully");
+  });
+
+  test("should upload a test file on a hidden input field ", async ({
+    page,
+  }) => {
+    // Open Url
+    await page.goto("https://practice.sdetunicorns.com/cart/");
+
+    //provide test file path
+    const filePath = path.join(__dirname, "../data/logo.png");
+
+    //Dom manipulation
+    await page.evaluate(() => {
+      const selector = document.querySelector("input#upfile_1");
+      // if you have the selector -> set the className to empty
+      if (selector) {
+        selector.className = "";
+      }
+    });
+
+    // Upload test file
+    await page.setInputFiles("input#upfile_1", filePath); //throws error
+
+    //Click submit button
+    await page.locator("#upload_1").click();
+
+    //Assertion
+    await expect(
+      page.locator("#wfu_messageblock_header_1_label_1")
+    ).toContainText("uploaded successfully");
+  });
+});
