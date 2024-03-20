fe0222ada372c3e666d3e13c2031243a64f7372f
Minh Vo <minhvo4398@gmail.com>
2024-03-20 20:48:13 +0700

    change git hubaction

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,75 +1,112 @@
-import { defineConfig, devices } from "@playwright/test";
+import type { PlaywrightTestConfig } from '@playwright/test';
+import { devices } from '@playwright/test';
 
-export default defineConfig({
-  testDir: "./tests",
-  // Maximum time one test can run for
+/**
+ * Read environment variables from file.
+ * https://github.com/motdotla/dotenv
+ */
+// require('dotenv').config();
+
+/**
+ * See https://playwright.dev/docs/test-configuration.
+ */
+const config: PlaywrightTestConfig = {
+  testDir: './tests',
+  /* Maximum time one test can run for. */
   timeout: 30 * 1000,
-  // Run tests in files in parallel
-  fullyParallel: true,
-  forbidOnly: !!process.env.CI,
-  //Retry on CI only
-  retries: process.env.CI ? 2 : 0,
-  // Opt out of parallel tests on CI
-  workers: process.env.CI ? 1 : undefined,
-  reporter: [["line"], ["allure-playwright"]],
-
-  globalSetup: require.resolve("./utils/global-setup"),
-  use: {
-    trace: 'retain-on-failure',
-    headless: false,
-    // Base URL to use in actions like 'await page.goto('/)'
-    baseURL: "https://practice.sdetunicorns.com/",
-    storageState: "loggedInState.json",
-  },
   expect: {
-    // Maximum time expect() should wait for the condition to be match
-    //Ex: in await expect(locator).toHaveText();
-    timeout: 5000,
+    /**
+     * Maximum time expect() should wait for the condition to be met.
+     * For example in `await expect(locator).toHaveText();`
+     */
+    timeout: 5000
+  },
+  /* Run tests in files in parallel */
+  fullyParallel: true,
+  /* Fail the build on CI if you accidentally left test.only in the source code. */
+  forbidOnly: !!process.env.CI,
+  /* Retry on CI only */
+  retries: process.env.CI ? 2 : 0,
+  /* Opt out of parallel tests on CI. */
+  workers: process.env.CI ? 1 : undefined,
+  /* Reporter to use. See https://playwright.dev/docs/test-reporters */
+  reporter: [['allure-playwright', {outputFolder: 'test-results'}], ['line']],
+
+  globalSetup: require.resolve('./utils/global-setup'),
+
+  /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
+  use: {
+    /* Maximum time each action such as `click()` can take. Defaults to 0 (no limit). */
+    actionTimeout: 0,
+    /* Base URL to use in actions like `await page.goto('/')`. */
+    baseURL: 'https://practice.automationbro.com',
+
+    /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
+    trace: 'retain-on-failure',
+
+    storageState: 'loggedInState.json'
   },
 
   /* Configure projects for major browsers */
   projects: [
     {
-      name: "chromium",
-      use: { ...devices["Desktop Chrome"], headless: false },
+      name: 'chromium',
+      use: {
+        ...devices['Desktop Chrome'],
+      },
     },
 
-    {
-      name: "firefox",
-      use: { ...devices["Desktop Firefox"], headless: false },
-    },
+    // {
+    //   name: 'firefox',
+    //   use: {
+    //     ...devices['Desktop Firefox'],
+    //   },
+    // },
 
-    {
-      name: "webkit",
-      use: { ...devices["Desktop Safari"], headless: false },
-    },
-    /*
+    // {
+    //   name: 'webkit',
+    //   use: {
+    //     ...devices['Desktop Safari'],
+    //   },
+    // },
 
     /* Test against mobile viewports. */
     // {
     //   name: 'Mobile Chrome',
-    //   use: { ...devices['Pixel 5'] },
+    //   use: {
+    //     ...devices['Pixel 5'],
+    //   },
     // },
     // {
     //   name: 'Mobile Safari',
-    //   use: { ...devices['iPhone 12'] },
+    //   use: {
+    //     ...devices['iPhone 12'],
+    //   },
     // },
 
     /* Test against branded browsers. */
     // {
     //   name: 'Microsoft Edge',
-    //   use: { ...devices['Desktop Edge'], channel: 'msedge' },
+    //   use: {
+    //     channel: 'msedge',
+    //   },
     // },
     // {
     //   name: 'Google Chrome',
-    //   use: { ...devices['Desktop Chrome'], channel: 'chrome' },
+    //   use: {
+    //     channel: 'chrome',
+    //   },
     // },
   ],
+
+  /* Folder for test artifacts such as screenshots, videos, traces, etc. */
+  // outputDir: 'test-results/',
 
   /* Run your local dev server before starting the tests */
   // webServer: {
   //   command: 'npm run start',
-  //   url: 'http://127.0.0.1:3000',
-  //   reuseExistingServer: !process.env.CI,
+  //   port: 3000,
   // },
-});
+};
+
+export default config;
